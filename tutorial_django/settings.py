2448c5448be087845c7886fd08ca0c8c61e7b3b1
Alejandro Estévez Martínez <estevezmartinez.alejandro@gmail.com>
2020-09-18 10:35:10 +0200

    Fix: Comillas

--- tutorial_django/settings.py
+++ tutorial_django/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = "tutorial_django.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": [f"{BASE_DIR}/db.sqlite3"],
+        "NAME": f"{BASE_DIR}/db.sqlite3",
     }
 }
 
